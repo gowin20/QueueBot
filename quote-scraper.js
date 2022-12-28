61460ac154b3a8273202e50d729a2c602ecee245
George Owen <gowen@esri.com>
2022-12-27 22:32:37 -0600

    bot base

--- quote-scraper.js
+++ quote-scraper.js
@@ -1,36 +1,26 @@
 const fs = require('fs');
-const http = require('http');
-const jsdom = require("jsdom");
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 async function startServer() {
-    fs.readFile("./sbeve-quotes.html",'utf8',(err,data) => {
+    fs.readFile('./sbeve-quotes.html', 'utf8', (err, data) => {
         if (err) {
-            console.log("broken")
+            console.log('eew');
         }
 
         const { document } = (new JSDOM(data)).window;
-        console.log(document)
-        const quotes = document.getElementsByClassName("chatlog__markdown-quote-content");
+        console.log(document);
+        const quotes = document.getElementsByClassName('chatlog__markdown-quote-content');
 
-        let quoteContent = []
-        for (quote of quotes) {
+        const quoteContent = [];
+        for (const quote of quotes) {
             quoteContent.push(quote.innerHTML);
         }
-        let output = JSON.stringify(quoteContent);
-        console.log(output)
-        fs.writeFileSync('brad-quotes.json',output);
-        /*
-        http.createServer(function(request,response) {
-            response.writeHeader(200, {"Content-Type": "text/html"});
-            response.write(data);
-            response.end();
-        }).listen(8000);
-
-        console.log("server started)")
-        */
-    })
-    return
+        const output = JSON.stringify(quoteContent);
+        console.log(output);
+        fs.writeFileSync('brad-quotes.json', output);
+    });
+    return;
 }
 
-startServer()
+startServer();
--- random-quote.js
+++ random-quote.js
@@ -1,11 +1,9 @@
-const fs = require("fs");
+const fs = require('fs');
 
-let raw = fs.readFileSync('brad-quotes.json');
+const raw = fs.readFileSync('brad-quotes.json');
 
-let quotes = JSON.parse(raw);
+const quotes = JSON.parse(raw);
 
 
-
-let rand = Math.floor(Math.random() * quotes.length)
-//console.log(quotes.length)
-console.log(quotes[rand])
+const rand = Math.floor(Math.random() * quotes.length);
+console.log(quotes[rand]);
